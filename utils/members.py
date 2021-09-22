7f54ffdbad2cd52f1213e5b3d4c1cf5529572604
dpalatynski <57257514+dpalatynski@users.noreply.github.com>
2021-09-22 23:34:29 +0200

    v0.5 user info and avatars
    .user [user mentioned] information about user account and .avatar to view someone's avatar

--- utils/members.py
+++ utils/members.py
@@ -1,4 +1,5 @@
 from discord.ext import commands
+from discord import Embed, Member
 
 
 class Members(commands.Cog):
@@ -21,6 +22,77 @@ class Members(commands.Cog):
             reply = 'There are {} members of {}.'.format(len(ctx.guild.members), ctx.guild)
 
         await ctx.send(reply)
+
+    @commands.command(name='user',
+                      brief='Who are you?',
+                      description='-> ".user " - information about your Discord account \n'
+                                  '-> ".user [user]" - information about specific Discord account')
+    async def user(self, ctx, user: Member = None):
+        if user is None:
+            user = ctx.message.author
+        embed = Embed(title=user.display_name, color=0x00ff00)
+        embed.set_thumbnail(url=user.avatar_url)
+        embed.add_field(name='Nickname', value=user.name + '#' + user.discriminator, inline=False)
+        embed.add_field(name='Joined At', value=user.joined_at.date())
+        embed.add_field(name='Created At', value=user.created_at.date())
+        embed.add_field(name='Roles:', value=get_roles(user.roles), inline=False)
+        if user.premium_since is not None:
+            embed.add_field(name=':gem: Server Boosting since:', value=user.premium_since.date())
+        if len(user.activities) != 0:
+            embed.add_field(name='Activites:', value=get_activities(user.activities), inline=False)
+        embed.set_footer(text='User ID: ' + str(user.id))
+
+        await ctx.send(embed=embed)
+
+    @user.error
+    async def user_eror(self, ctx, error):
+        if error:
+            await ctx.send('Unable to find information about this account.')
+
+    @commands.command(name='avatar',
+                      brief='View an avatar',
+                      description='-> ".avatar " - look at your Discord avatar \n'
+                                  '-> ".avatar [user]" - look at user\'s Discord avatar')
+    async def avatar(self, ctx, user: Member = None):
+        if user is None:
+            user = ctx.message.author
+        embed = Embed(title=user.display_name, color=0x00ff00)
+        embed.set_image(url=user.avatar_url)
+        embed.set_footer(text=user.name + '#' + user.discriminator)
+
+        await ctx.send(embed=embed)
+
+    @user.error
+    async def avatar_eror(self, ctx, error):
+        if error:
+            await ctx.send('Unable to look at this avatar.')
+
+
+def setup(client):
+    client.add_cog(Members(client))
+
+
+def get_roles(roles):
+    message = ''
+    if len(roles) != 0:
+        for role in roles[1:]:
+            message += role.name + ', '
+    else:
+        message = 'No roles assigned!!!'
+
+    return message[:-2]
+
+
+def get_activities(activities):
+    message = ''
+    for activity in activities:
+        if hasattr(activity, 'track_id'):
+            message += ':headphones:  Listening: ' + activity.artist + ' - ' + activity.title + ' on Spotify \n'
+        if hasattr(activity, 'application_id'):
+            message += ':video_game:  Playing: ' + activity.name + ' \n'
+
+    return message
+
 
 
 def setup(client):
