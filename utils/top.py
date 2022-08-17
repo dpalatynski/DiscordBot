bc847e683b7a06d49fce20df21c6f63a78d58271
dpalatynski <57257514+dpalatynski@users.noreply.github.com>
2022-08-17 22:21:31 +0200

    v1.19 top lookup - 1st part of upgrade top lookups

--- utils/top.py
+++ utils/top.py
@@ -1,6 +1,14 @@
 from discord.ext import commands
-import time
+from utils.functions import check_for_channel
 import datetime
+
+
+LOOKUP_TYPES = {'today': None,
+                'yesterday': None,
+                'week': None,
+                'month': None,
+                'year': None,
+                'all': None}
 
 
 class Top(commands.Cog):
@@ -17,80 +25,26 @@ class Top(commands.Cog):
                                   'a mentioned channel \n'
                                   '-> ".top [channel] [limit]" - returns a limited leaderboard of users with the most '
                                   'messages sent in a mentioned channel \n')
-    async def top(self, ctx, channel=None, limit=None):
-        try:
-            limit = int(limit)
-        except ValueError and TypeError:
-            limit = False
+    async def top(self, ctx, *args):
+        channel, limit, lookup_type = None, None, None
+        for arg in args:
+            if check_for_channel(arg):
+                channel = arg
+            if arg.isdigit():
+                limit = int(arg)
+            if arg in LOOKUP_TYPES:
+                lookup_type = arg
 
         messages = []
         members = set()
         results = []
 
-        start = time.time()
-        print('Executing...')
-
-        if channel == 'all':
-            await ctx.send(":warning: Warning! If your server is large enough,"
-                           " calculations could take few hours. :warning: ")
+        if lookup_type == 'all':
             for _channel in ctx.guild.text_channels:
                 async for message in _channel.history(limit=None):
-                    if message.author.bot is not True:
-                        members.add(message.author.id)
-                        messages.append(message.author.id)
-        else:
-            _channel = self.client.get_channel(int(channel[2:-1])) if channel else ctx.channel
-
-            async for message in _channel.history(limit=None):
-                if message.author.bot is not True:
-                    members.add(message.author.id)
-                    messages.append(message.author.id)
-        for member in members:
-            counter = messages.count(member)
-            try:
-                results.append([member, counter, self.client.get_user(member).name,
-                                self.client.get_user(member).discriminator])
-            except AttributeError:
-                pass
-
-        results = sorted(results, key=lambda x: x[1], reverse=True)
-        limit = int(limit) if (limit and 0 < int(limit) < len(results) and type(int(limit)) == int) else len(results)
-
-        messages = create_message(ctx, results, channel, _channel, '', limit)
-
-        end = time.time()
-        print('Finished. Time of execution: {} seconds.'.format(round(end - start, 2)))
-
-        for message in messages:
-            await ctx.send(message)
-
-    @commands.command(name='dailytop',
-                      brief='Leaderboard of messages sent today',
-                      description='-> ".dailytop" - returns a leaderboard of users with the most messages sent today '
-                                  'in this channel \n'
-                                  '-> ".dailytop all" - returns a leaderboard of users with the most messages sent '
-                                  'today in this server \n'
-                                  '-> ".dailytop [channel] - returns a leaderboard of users with the most messages '
-                                  'sent today in a mentioned channel \n'
-                                  '-> ".dailytop [channel] [limit]" - returns a limited leaderboard of users with the '
-                                  'most messages sent today in a mentioned channel \n')
-    async def dailytop(self, ctx, channel=None, limit=None):
-        try:
-            limit = int(limit)
-        except ValueError and TypeError:
-            limit = False
-
-        messages = []
-        members = set()
-        results = []
-
-        start = time.time()
-        print('Executing...')
-
-        if channel == 'all':
-            for _channel in ctx.guild.text_channels:
-                async for message in _channel.history(limit=None):
+                    print(message.created_at)
                     if message.created_at > datetime.datetime.today().replace(hour=0, minute=0, second=0):
+                        print(message.created_at)
                         if message.author.bot is not True:
                             members.add(message.author.id)
                             messages.append(message.author.id)
@@ -98,9 +52,9 @@ class Top(commands.Cog):
                         break
         else:
             _channel = self.client.get_channel(int(channel[2:-1])) if channel else ctx.channel
-
             async for message in _channel.history(limit=None):
-                if message.created_at > datetime.datetime.today().replace(hour=0, minute=0, second=0):
+                message_time = message.created_at + datetime.timedelta(hours=2)
+                if message_time > datetime.datetime.today().replace(hour=0, minute=0, second=0):
                     if message.author.bot is not True:
                         members.add(message.author.id)
                         messages.append(message.author.id)
@@ -116,13 +70,9 @@ class Top(commands.Cog):
                 pass
 
         results = sorted(results, key=lambda x: x[1], reverse=True)
-        limit = int(limit) if (limit and 0 < int(limit) < len(results) and type(int(limit)) == int) else len(
-            results)
+        limit = int(limit) if (limit and 0 < int(limit) < len(results) and type(int(limit)) == int) else len(results)
 
-        messages = create_message(ctx, results, channel, _channel, 'today', limit)
-
-        end = time.time()
-        print('Finished. Time of execution: {} seconds.'.format(round(end - start, 2)))
+        messages = create_message(ctx, results, channel, _channel, '', limit)
 
         for message in messages:
             await ctx.send(message)
