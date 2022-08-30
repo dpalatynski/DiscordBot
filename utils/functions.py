9d6b7a9154ac1afd067184574785ab0a5d2e650f
dpalatynski <57257514+dpalatynski@users.noreply.github.com>
2022-08-30 18:51:40 +0200

    v1.20 function to collect urls to memes

--- utils/functions.py
+++ utils/functions.py
@@ -7,3 +7,18 @@ def check_for_channel(channel_id):
         is_channel = False
 
     return is_channel
+
+
+def save_meme_url_to_file():
+    url = 'https://jbzd.com.pl/'
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, features="lxml")
+    images = soup.find_all("img", {"class": "article-image"})
+    img = random.choice(images)
+
+    with open(str(Path(os.getcwd()).parent) + "\configs\meme_urls.txt", "r") as file:
+        current_urls = file.readlines()
+
+    if img['src']+'\n' not in current_urls:
+        with open(str(Path(os.getcwd()).parent) + "\configs\meme_urls.txt", "a") as file:
+            file.write(img['src'] + '\n')
